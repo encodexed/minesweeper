8b9b3d021de5ed2614247c6b7b7405618f823afa
Robbie Gollan <robbiegollan@gmail.com>
2023-10-13 21:52:35 +1100

    adds time keeping logic

--- src/Utils.java
+++ src/Utils.java
@@ -2,6 +2,33 @@ import java.util.Date;
 
 public class Utils {
 
+  public static boolean processCommand(String command, Grid grid) throws OutOfBoundsError, Exception {
+    switch (command) {
+      case "new":
+        System.out.println("Starting a new game");
+        grid.printGrid();
+        return true;
+      case "grid":
+        grid.printGrid();
+      case "help":
+        printHelp();
+        return true;
+      case "time":
+        printTimeTaken(grid);
+        return true;
+      case "quit":
+        System.out.println("Quitting the game. Thanks for playing!");
+        return false;
+      default:
+        if (command.charAt(0) == '!') {
+          System.out.println(String.format("Flagging %s", command.substring(1)));
+        } else {
+          System.out.println(String.format("Revealing %s", command));
+        }
+        return true;
+    }
+  }
+
   public static void printHelp() {
     System.out.println("------------------------------------");
     System.out.println("Here are the accepted command patterns");
@@ -15,30 +42,17 @@ public class Utils {
     System.out.println("------------------------------------");
   }
 
-  public static boolean processCommand(String command, Grid grid) throws OutOfBoundsError, Exception {
-    switch (command) {
-      case "new":
-        System.out.println("Starting a new game");
-        grid.printGrid();
-        return true;
-      case "grid":
-        grid.printGrid();
-      case "help":
-        printHelp();
-        return true;
-      case "time":
-        System.out.println(new Date());
-        return true;
-      case "quit":
-        System.out.println("Quitting the game. Thanks for playing!");
-        return false;
-      default:
-        if (command.charAt(0) == '!') {
-          System.out.println(String.format("Flagging %s", command.substring(1)));
-        } else {
-          System.out.println(String.format("Revealing %s", command));
-        }
-        return true;
-    }
+  private static void printTimeTaken(Grid grid) {
+    Date now = new Date();
+    Date startedAt = grid.getTimeStarted();
+    long millisecondsTaken = now.getTime() - startedAt.getTime();
+    System.out.println(timeTakenFormatted(millisecondsTaken));
+  }
+
+  public static String timeTakenFormatted(long millisecondsTaken) {
+    long seconds = millisecondsTaken / 1000;
+    long minutes = (seconds % 3600) / 60;
+    long secondsLeft = ((seconds % 3600) % 60) % 60;
+    return String.format("Time taken: %02d:%02d", minutes, secondsLeft);
   }
 }
